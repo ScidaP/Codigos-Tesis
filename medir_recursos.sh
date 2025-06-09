3e36d5fbfc830573b0be54216967f50139c1e088
Patricio Scidá <patricioscida@gmail.com>
2025-06-09 12:00:25 -0300

    se coloca la IP del sv

--- medir_recursos.sh
+++ medir_recursos.sh
@@ -16,7 +16,7 @@ echo "Medición de Almacenamiento (fio)..."
 fio --name=test-readwrite --rw=randrw --bs=4k --size=100M --numjobs=1 --runtime=10 --group_reporting > resultados/almacenamiento_fio.txt
 
 echo "Medición de Red..."
-iperf3 -c <IP_SERVIDOR> -t 10 > resultados/red_iperf.txt 2>&1
+iperf3 -c 192.168.100.15 -t 10 > resultados/red_iperf.txt 2>&1
 mtr -r -c 10 8.8.8.8 > resultados/red_mtr.txt
 
 echo "Mediciones completas. Los resultados están en la carpeta 'resultados/'."
